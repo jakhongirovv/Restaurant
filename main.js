9521110e54ce229be3db25afcf8989e8df4bed3e
jakhongirovv <akromjakhongirov1@gmail.com>
2022-08-15 19:10:12 +0500

    Restaurant1

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 let odam;
 let korzinOff= [];
-function keldi(a) {
+function keldi() {
     $.ajax("http://myjson.dit.upm.es/api/bins/5rnc",{
         success: (qiymat) =>{
-            console.log("keldi qiymatin", qiymat);
-            odam=qiymat;
+            console.log(qiymat);
+            odam = qiymat;
             ekran(odam)
         },
         error: (ero) =>{
@@ -12,12 +12,12 @@ function keldi(a) {
         }
     })
 }
-keldi(odam)
+keldi()
 
 
-function ekran(a) {
+function ekran(h) {
     $("#row").html("")
-    a.map((v,i) =>{
+    h.map((v,i) =>{
         let col=`
         <div class="col-3 offset-1">
                 <div class="card bg-dark">
@@ -48,8 +48,6 @@ function ekran(a) {
         $("#row").append(col)
     })
 }
-
-ekran(odam)
 
 function del(i) {
     odam.splice(i,1)
@@ -94,30 +92,68 @@ function korzina_ekran(a) {
 
 
 
-// let odamcha;
-// function kel(a) {
-//     $.ajax("http://myjson.dit.upm.es/api/bins/ceug", {
-//         success: (qiyma) =>{
-//             console.log("qiymatin keldi",qiyma);
-//             odamcha=qiyma
-//             oyna(kel)
-//         },
-//         error: (er) =>{
-//             console.log(er);
-//         }
-//     })
-// }
+let odamcha;
 
-// kel(odamcha)
+function kel(a) {
+    $.ajax("http://myjson.dit.upm.es/api/bins/edws", {
+        success: (qiyma) =>{
+            console.log("qiymatin keldi",qiyma);
+            odamcha=qiyma
+            oyna(odamcha)
+        },
+        error: (er) =>{
+            console.log(er);
+        }
+    })
+}
+
+kel(odamcha)
 
 
 function oyna(a) {
     $("#rowa").html("")
-    a.map((s,i) =>{
+    a.map((v,i) =>{
         let coool =`
-        
+        <div class="col-3" offset="col-1">
+                        <img class="foot_img" src="${v.photo}" width="250" height="200" alt="rasiml">
+                        <b class="text-light d-block">${v.name}</b>
+                        <p class="text-light">${v.ovqat}</p>
+                        <p class="text-light">${v.money}</p>
+
+                        <div class="bg-dark" onclick="dele(${i})">
+                            <button class="bg-dark buto mt-4">
+                                <span class="text-light"><i class="fa-solid fa-trash-can"></i></span>
+                                 </button>
+                                 <button class="bg-dark buto  mt-4" onclick="korzins(${i})">
+                                 <span class="${i} text-light"><i class="fa-solid fa-trash-arrow-up"></i></span>
+                                 </button>
+                                 <button class="bg-dark buto  mt-4" onclick="likes(${i})">
+                                 <span class="${v.like ? "text-danger" : "d-none"}"><i class="fa-solid fa-thumbs-up"></i></span>
+                                <span class="${v.like ? "d-none" : "text-danger"}"><i class="fa-solid fa-thumbs-down"></i></span>
+                                </button>
+                        </div>
+                    </div>
         `
         $("#rowa").append(coool)
     })
 }
 
+
+function dele(i) {
+    odam.splice(i,1)
+    ekran(odam)
+}
+
+function likes(i) {
+    odam[i].like = !odam[i].like
+    ekran(odam)
+}
+
+function korzins(i) {
+    korzinOff.push(odam[i]);
+    korzina_ekran(korzinOff);
+    let o = korzinOff.length;
+
+    $("#korzna").html(o)
+
+}
